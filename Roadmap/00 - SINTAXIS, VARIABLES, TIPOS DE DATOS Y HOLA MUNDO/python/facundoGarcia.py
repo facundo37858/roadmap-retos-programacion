950f4fc37151b47e0fe3643aa902b0bb7e040719
facundo37858 <facundoomargarcia@gmail.com>
2024-08-14 18:02:13 -0300

    comentarios y variables

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/facundoGarcia.py
@@ -0,0 +1,29 @@
+## Python https://www.python.org/
+
+# Tipos de comentarios:
+    # En una linea
+    # Multiples lineas no existe en python lo que se utiliza es
+""" Comentario multiple
+    Es una cadena que no está asignada a ninguna variable,
+    por lo que su programa no la llama ni hace referencia a ella.
+    Aun así, dado que se ignorará en el tiempo de ejecución y 
+    no aparecerá en el código de bytes, puede actuar efectivamente como un comentario.
+https://realpython.com/python-comments-guide/ 
+"""
+'''
+Comentario multiple
+'''
+# Variables
+    # Numericas
+numerica =10
+    # Texto
+texto = 'Texto'
+    # Booleanas
+booleanas = True
+    # Flotantes
+flotantes = 2.3
+    # Complejas
+complejos = 2+9j
+
+# Imprimir
+print("Hola, python.")
